8ffe7ab18950b27c45a5426ab583f65b5dd4b13e
Arii <223894237+jaiden3434@users.noreply.github.com>
2026-08-03 10:29:12 -0500

    Revert "Made a working Enemy spawner"
    This reverts commit 2ca1299e4452e1039a97468dad0efb4c06d3e086.

--- Assets/Scripts/enemy_spawner.gd
+++ Assets/Scripts/enemy_spawner.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-@export_file("*.tscn") var enemy_path
+@export var Enemy : PackedScene
 
 func _ready() -> void:
-	var enemy = load(enemy_path).instantiate()
-	enemy.position = position
-	add_child(enemy)
-	
+	Enemy.instantiate()
